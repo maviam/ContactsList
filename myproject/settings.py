94c114e4dd9fd28d1fbe1471d9a1360414da30ab
Marcelo Amorim <marceloamorim.fa@gmail.com>
2025-04-21 10:01:04 +0100

    Update initial project

--- myproject/settings.py
+++ myproject/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / 'base_static' ]
 STATIC_ROOT = BASE_DIR / 'static'
-MEDIA_URL = "media/"
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
