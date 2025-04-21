41bbf197a49cf96efe9518ae805954466110bcff
Marcelo Amorim <marceloamorim.fa@gmail.com>
2025-04-21 12:38:31 +0100

    CSS Code and Faker

--- contact/admin.py
+++ contact/admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from contact import models
 
 # Register your models here.
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('-id',)
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id','first_name','last_name','phone')
-    ordering= ('-first_name',)
+    ordering = ('-first_name',)
     list_filter = ('created_date',) # Aparece à direita algumas possibilidades de filtros
     # search_fields = ('first_name', 'phone',) # Adiciona uma campo de pesquisa
-    list_per_page = 2 # Número de registos por página
+    list_per_page = 100 # Número de registos por página
     list_max_show_all = 200 # Máximo de registos em uma página
     # list_editable = ('last_name',) # Lista de campos editáveis
     list_display_links = ('first_name',) # Campo com hiperligação direta para a edição do contato
--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
 class Contact(models.Model):
     # CharField é um campo de preenchimento obrigatório
     first_name = models.CharField(max_length=20)
@@ -14,6 +22,9 @@ class Contact(models.Model):
     # e o TIMEZONE para 'Europe/Lisbon'
     description = models.TextField(blank=True)
     picture = models.ImageField(blank=True,upload_to='pictures/%Y/%m/')
-
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,blank=True,null=True)
+    show = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    
     def __str__(self):
         return f'{self.first_name} {self.last_name} ({self.email})'
