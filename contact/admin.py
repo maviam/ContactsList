ff3364536909c5472747b9dda2e111ef525342e4
Marcelo Amorim <marceloamorim.fa@gmail.com>
2025-04-29 23:05:06 +0100

    Search Bar

--- contact/admin.py
+++ contact/admin.py
@@ -9,7 +9,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id','first_name','last_name','phone')
+    list_display = ('id','first_name','last_name','phone','show')
     ordering = ('-first_name',)
     list_filter = ('created_date',) # Aparece à direita algumas possibilidades de filtros
     # search_fields = ('first_name', 'phone',) # Adiciona uma campo de pesquisa
--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
-    c = Contact.objects.all()
+    # c = Contact.objects.all()
+    c = Contact.objects.filter(show=True).order_by('-id')[0:10]
     context = {
-        'contacts': c
+        'contacts': c,
+        'site_title': 'Contacts List - '
     }
     return render (
+        request,
+        'contact/index.html',
+        context
+    )
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(id=contact_id).first()
+    single_contact = get_object_or_404(Contact.objects, id=contact_id,show=True)
+    context = {
+        'contact': single_contact,
+        'site_title': f'{single_contact.first_name} {single_contact.last_name} -'
+    }
+    return render (
+        request,
+        'contact/contact.html',
+        context
+    )
+
+def search(request):
+    # search_value = request.GET
+    # print(search_value)
+    search_value = request.GET.get('q','').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    # https://docs.djangoproject.com/en/5.0/ref/models/lookups/
+    # contacts = Contact.objects.filter(show=True).filter(first_name__icontains=search_value).order_by('-id')[0:11]
+    contacts = Contact.objects.filter(show=True).filter(Q(first_name__icontains=search_value) | Q(last_name__icontains=search_value)).order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos -'
+    }
+
+    return render(
         request,
         'contact/index.html',
         context
